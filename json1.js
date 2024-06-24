7b90e267ad7496fbe57c7d55952d167bba1bd93f
Manggla52 <Michaelmartinez427@gmail.com>
2024-06-24 08:14:20 -0500

    updated

--- json1.js
+++ json1.js
@@ -1,12 +1,17 @@
 
+function()swapData{
+let nameHeader;
+let nameParagraph;
+let nameParagraph2;
 let userName;
 
-userName = prompt("What is your name?");
-document.write(`Hello ${userName}, nice to meet you! Welcome to the about section of S & M Bully Camp.`);
+nameHeader = prompt(`what is you name?`);
+nameHeader.innerHTML = `Hello ${userName}, nice to meet you! Welcome to the about section of S & M Bully Camp.`);
 let dogName;
         
 dogName = prompt(`${userName}`, "What is the breed of your dog?");
-
+}
 document.write()
 // Try it! Write a message about the dog's name!
 document.write(`${userName}, ${dogName}, is a great breed of dog!`);
+greetButtn.onclick = swapData;
